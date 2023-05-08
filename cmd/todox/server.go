09df71493d8449098777b9475e932949d6573786
Antonio Pagano <apagano@wawand.co>
2023-05-08 17:24:29 -0500

    task: adding a message when the serve starts

---
+++ cmd/todox/server.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"net/http"
+	"paganotoni/todox/database"
+	"paganotoni/todox/internal/fs"
+	"paganotoni/todox/public"
+	"paganotoni/todox/todo"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func buildServer() http.Handler {
+	router := chi.NewRouter()
+
+	router.Use(middleware.RequestID)
+	router.Use(middleware.RealIP)
+	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
+	router.Use(database.Connection)
+
+	router.Get("/", todo.Index)
+	router.Get("/search", todo.Search)
+	router.Get("/{id}/edit", todo.Edit)
+	router.Post("/", todo.Create)
+	router.Delete("/{id}", todo.Delete)
+	router.Put("/{id}", todo.Update)
+	router.Put("/{id}/complete", todo.Complete)
+
+	// Static files like css, images and so on.
+	// TODO: review name of the folder
+	publicFolder := http.FS(fs.NewFallback(public.Folder, "public/"))
+	router.Handle("/*", http.StripPrefix("/", http.FileServer(publicFolder)))
+
+	return router
+}
