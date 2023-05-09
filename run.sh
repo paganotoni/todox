7b6bb808f0d06c4fa88b7e27a183f07306f30bea
Antonio Pagano <apagano@wawand.co>
2023-05-09 17:49:35 -0500

    task: changing the endpoint

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 # Always restore before starting
-litestream restore -o /bin/todox.db s3://f72ffa7fed95c214a8bcc247a3e59805.r2.cloudflarestorage.com/todox
+litestream restore -o /bin/todox.db https://f72ffa7fed95c214a8bcc247a3e59805.r2.cloudflarestorage.com/todox
 
 # Migrate the database
 /bin/tools migrate
 
 # Start sever and backup
-litestream replicate -exec "/bin/app" /bin/todox.db s3://f72ffa7fed95c214a8bcc247a3e59805.r2.cloudflarestorage.com/todox
+litestream replicate -exec "/bin/app" /bin/todox.db https://f72ffa7fed95c214a8bcc247a3e59805.r2.cloudflarestorage.com/todox
