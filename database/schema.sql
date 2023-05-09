0e05e59ee8945e752aced707f2b8ccc644334f94
Antonio Pagano <apagano@wawand.co>
2023-05-09 18:07:25 -0500

    task: considering fil;e

--- database/schema.sql
+++ database/schema.sql
@@ -1,4 +1,6 @@
 PRAGMA journal_mode = wal;
+PRAGMA busy_timeout = 5000;
+PRAGMA synchronous = NORMAL;
 
 CREATE TABLE IF NOT EXISTS "todos" (
     "id" TEXT PRIMARY KEY,
--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 # Always restore before starting
-litestream restore
+litestream restore -config /etc/litestream.yml
 
 # Migrate the database
 /bin/tools migrate
 
 # Start sever and backup
-litestream replicate -exec "/bin/app" /bin/todox.db
+litestream replicate -exec "/bin/app" -config /etc/litestream.yml
