653ccbaa5d0de8bf4c67545324876c512a0fbab6
Antonio Pagano <apagano@wawand.co>
2023-05-09 17:45:17 -0500

    task: adding the correct database file

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 # Always restore before starting
-litestream restore -o /bin/cach.db s3://f72ffa7fed95c214a8bcc247a3e59805.r2.cloudflarestorage.com/todox
+litestream restore -o /bin/todox.db s3://f72ffa7fed95c214a8bcc247a3e59805.r2.cloudflarestorage.com/todox
 
 # Migrate the database
 /bin/tools migrate
 
 # Start sever and backup
-litestream replicate -exec "/bin/app" /bin/cach.db s3://f72ffa7fed95c214a8bcc247a3e59805.r2.cloudflarestorage.com/todox
+litestream replicate -exec "/bin/app" /bin/todox.db s3://f72ffa7fed95c214a8bcc247a3e59805.r2.cloudflarestorage.com/todox
