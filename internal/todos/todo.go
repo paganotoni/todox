99c9618a282a5c12b51efac4b9bc7a8bf76fd81d
Antonio Pagano <apagano@wawand.co>
2024-05-14 19:07:33 -0500

    removing SVG comment

--- internal/todos/todo.go
+++ internal/todos/todo.go
@@ -18,11 +18,6 @@ func todoHTML(t Instance) ElementRenderer {
 			!t.Completed,
 			SPAN().Attr("hx-get", "/"+t.ID.String()+"/edit").Attr("hx-target", "closest li").Attr("hx-swap", "outerHTML").CLASS("cursor-pointer underline flex flex-row gap-2 items-center").Attr("_", "on htmx:afterRequest if detail.successful tell #content set you.value to ''").Children(
 				SPAN().Text(t.Content),
-				//TODO Icon
-				// <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-				//                 <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
-				//             </svg>
-				//
 				SVG_SVG().Attr("xmlns", "http://www.w3.org/2000/svg").Attr("fill", "none").Attr("viewBox", "0 0 24 24").Attr("stroke-width", "1.5").Attr("stroke", "currentColor").CLASS("w-4 h-4").Children(
 					SVG_PATH().Attr("stroke-linecap", "round").Attr("stroke-linejoin", "round").Attr("d", "M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"),
 				),
