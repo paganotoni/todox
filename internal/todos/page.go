d75c776af61d1e6a7677ed9a6e465a434f3edd63
Antonio Pagano <apagano@wawand.co>
2024-06-24 17:12:11 -0400

    task: upgrading htmx

--- internal/todos/page.go
+++ internal/todos/page.go
@@ -8,8 +8,9 @@ func page(yield ElementRenderer) ElementRenderer {
 			META().NAME("viewport").CONTENT("width=device-width, initial-scale=1"),
 			META().CHARSET("utf-8"),
 			TITLE().Text("Todo"),
-			SCRIPT().SRC("https://unpkg.com/htmx.org@1.9.2"),
+			SCRIPT().SRC("https://unpkg.com/htmx.org@2.0.0/dist/htmx.min.js"),
 			SCRIPT().SRC("https://unpkg.com/hyperscript.org@0.9.8"),
+
 			LINK().REL("stylesheet").HREF("/public/application.css"),
 		),
 		BODY().CLASS("h-full bg-gray-100 pb-10 pt-10").Children(
